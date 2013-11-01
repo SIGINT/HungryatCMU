87157f0b2a32a1fac499d5f2dec768926d10e086
Luke Jasko <ltj@andrew.cmu.edu>
2013-10-31 23:14:19 -0400

    Updated settings.py for MySQL

--- webapps/webapps/settings.py
+++ webapps/webapps/settings.py
@@ -16,8 +16,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',                        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_ROOT + 'db/webapps.db',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'hungry',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
