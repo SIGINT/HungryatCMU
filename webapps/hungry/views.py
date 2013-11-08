9434a2f519c6cf918f59878b81025d67998d53ee
Luke Jasko <ltj@andrew.cmu.edu>
2013-11-08 05:28:34 -0500

    Imports for render & other functions missing

--- webapps/hungry/views.py
+++ webapps/hungry/views.py
@@ -1,13 +1,13 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect, get_object_or_404
 """
 from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import send_mail
-from django.core.urlresolvers import reverse
-from django.http import HttpResponse, Http404
-from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from datetime import datetime
 from mimetypes import guess_type
