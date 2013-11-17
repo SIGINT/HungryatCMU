b7f37205d005a9220a15d524a052e942ad6e19a9
Karthik Murugan <karthik.mcg@gmail.com>
2013-11-17 16:27:04 -0500

    Minor changes

--- HungryatCMU/HungryApp/models.py
+++ HungryatCMU/HungryApp/models.py
@@ -148,3 +148,4 @@ class Order(models.Model):
     is_delivery = models.BooleanField(default=False)
     status = models.CharField(max_length=2, choices=ORDER_STATUSES, default='NP')
     restaurant_id = models.ForeignKey(Restaurant,blank=True,null=True)
+
