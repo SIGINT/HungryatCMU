673f6e85e01294feb78fad58707a74645763e5a7
Luke Jasko <jasko.luke@gmail.com>
2016-09-25 20:20:15 -0400

    Reorganizing app directories - removing irrelevant docs

--- webapps/hungry/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-
-urlpatterns = patterns('',
-    url(r'^$', 'hungry.views.home'),
-    url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'hungry/login.html'}),
-    url(r'^logout$', 'django.contrib.auth.views.logout_then_login'),
-)
